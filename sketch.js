b1c3d48478c52044c38f9d7bd9ab8eba43b7d957
Sebastian Crandall <jocelynnecrandall@gmail.com>
2025-12-03 12:57:46 -0600

    routes added

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 let routeNum, routeName
+let i = 0
+let time = 0
+var showRoutes = false
 
 function preload(){
     busRoutes = loadTable("Bus_Route_20251127.csv", "header")
@@ -6,26 +9,48 @@ function preload(){
 
 function setup(){
     createCanvas(1000,600);
+    textSize(24);
+    textFont('courier new')
  
 }
+
 function draw(){
     background(200,180,130);
+       text('Baton Rouge CATS Bus Routes', width-400,30)
 
-    if(mouseIsPressed){
-        for (let i = 0; i < busRoutes.getRowCount(); i++) {
-            routeNum = busRoutes.getString(i, 'Route Number');
-            routeName = busRoutes.getString(i,'ROUTE NAME',);
+    if(key === "g"){
+        routesList();
+    } else {
+        text('Press G to show routes',10,30)
+    }
+}
 
-            let y = 30 + i * 30;
-            textSize(24)
-            textFont("courier new")
-            text(routeNum, 10,y);
-            text(routeName,60,y);
-            } 
-    } else{
-        textSize(24)
-        textFont('courier new')
-        text("Hold the mouse show available routes.",10,30)
-    }   
+function keyPressed() {
+    if(key == "g") {
+        i++;
+    }
+}
+
+function routesList(){
+    // time += 0.1
+    // if(time > 1){
+        // for (let i = 0; i < busRoutes.getRowCount(); i += 1){
+        routeNum = busRoutes.getString(i, 'Route Number');
+        routeName = busRoutes.getString(i,'ROUTE NAME');
+        geo = busRoutes.getString(i,'GEOMETRY');
+
+
+        // 
+
+        let y = 100 + i * 10;
+        // let y = 30;
+
+        text(routeNum, 20,y);
+        text(routeName,60,y);
+        text(geo,10,70);
+
+
+        // } 
+    // }
 
 }
